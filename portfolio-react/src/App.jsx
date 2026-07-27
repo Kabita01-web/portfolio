3bc32b582fba3f045610aefc7d137ee5b26ac7f2
Kabita Bhurtel <kabitabhurtel222@gmail.com>
2026-07-27 14:26:07 +0545

    feat: Add smooth scrolling to portfolio components

--- portfolio-react/src/App.jsx
+++ portfolio-react/src/App.jsx
@@ -1,24 +1,31 @@
-import { ThemeProvider } from './context/ThemeContext';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import About from './components/About';
-import Skills from './components/Skills';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-
+import { ThemeProvider } from "./context/ThemeContext";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import SmoothScroll from "./components/SmoothScroll";
+import IntroScreen from "./components/IntroScreen";
+import Highlights from "./components/Highlights";
 function App() {
   return (
     <ThemeProvider>
-      <Navbar />
-      <main>
-        <Hero />
-        <About />
-        <Skills />
-        <Projects />
-        <Contact />
-      </main>
-      <Footer />
+      <IntroScreen>
+        <SmoothScroll>
+          <Navbar />
+          <main>
+            <Hero />
+            <Highlights />
+            <About />
+            <Skills />
+            <Projects />
+            <Contact />
+          </main>
+          <Footer />
+        </SmoothScroll>
+      </IntroScreen>
     </ThemeProvider>
   );
 }
